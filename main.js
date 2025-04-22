26cced0302a640755af3890a8fa26d95beb2c30c
Denys Handziichuk <93343363+DenysHandziichuk@users.noreply.github.com>
2025-04-22 18:21:06 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function makeGreen(event) {
+    event.preventDefault()
+    document.body.style.backgroundColor = 'green'
+}
+
+function makeRed(event) {
+    event.preventDefault()
+    document.body.style.backgroundColor = 'red'
+}
+
+function makeBlue(event) {
+    event.preventDefault()
+    document.body.style.backgroundColor = 'blue'
+}
+
+function makeYellow(event) {
+    event.preventDefault()
+    document.body.style.backgroundColor = 'yellow'
+}
